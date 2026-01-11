530665be1e860e3b1551d0a6f17a2b9fd62ce957
Codered1729 <vishnutejagadadasu@gmail.com>
2026-01-11 20:50:45 +0530

    ESP32 relay control using sinric pro

---
+++ secrets_example.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Rename this file to secrets.h and fill in your values
+
+#define WIFI_SSID       "YOUR_WIFI_NAME"
+#define WIFI_PASSWORD   "YOUR_WIFI_PASSWORD"
+
+#define APP_KEY     "YOUR_APP_KEY"
+#define APP_SECRET  "YOUR_APP_SECRET"
+#define DEVICE_ID   "YOUR_DEVICE_ID"
